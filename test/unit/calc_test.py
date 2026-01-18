3d6cbbb4456576ba2213e1ba71221ff7c56de5f1
Andrea Sil <andreasilas.as@gmail.com>
2026-01-18 20:29:16 +0100

    Edit test

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -62,5 +62,18 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(0, self.calc.substract(0, 0))
         self.assertRaises(TypeError, self.calc.substract, "0", 0)
         
+    def test_divide_by_zero_raises(self):
+        with self.assertRaises(ZeroDivisionError): self.calc.divide(10, 0)
+        
+    def test_divide_by_zero_raises(self):
+        self.assertRaises(ZeroDivisionError, self.calc.divide, 10, 0)
+
+    def test_power_fails_with_nan_parameter_second_arg(self):
+        self.assertRaises(TypeError, self.calc.power, 2, "2")
+        
+    def test_convert_to_number_negative_float(self):
+        from app import util
+        self.assertEqual(-0.5, util.convert_to_number("-0.5"))
+        
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
